23f2c23276107db5042f06a0b16924cf606eccec
rhl494 <rahulsaxena494@yahoo.com>
2019-04-16 16:03:28 -0400

    Next Commit

--- chalkboard/src/main/java/com/springboot/main.java
+++ chalkboard/src/main/java/com/springboot/main.java
@@ -5,6 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class main {
     public static void main(String[] args) {
-        SpringApplication.run(MainApp.class, args);
+        SpringApplication.run(main.class, args);
     }
 }
